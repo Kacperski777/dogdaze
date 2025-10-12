cee54c62eb21f5f380b98d730dcdf0231136ffec
Kacper Sliwinski <kacpermmlp@gmail.com>
2025-10-12 11:50:51 +0100

    Init dogdaze setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-green-100">
+    <h1 class="text-1xl font-bold text-green-800">DogDaze is ready 🐾</h1>
+  </div>
+</template>
+
