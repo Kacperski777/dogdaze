20e7b31b639ac06afc23d4e1664a3ec9582060a5
Kacper Sliwinski <kacpermmlp@gmail.com>
2025-10-13 20:24:20 +0100

    added supabase + added formkit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import router from './router'
+import { plugin } from '@formkit/vue'
+import formkitConfig from './formkit.config.js'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(plugin, formkitConfig).mount('#app')
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,3 +1,3 @@
 <template>
-    Login
+    <FormKit type="text" label="Email" />
 </template>
